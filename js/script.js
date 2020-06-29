ee3cded94b5ff34dce3712500d2107bb661ffd84
Christopher Mc <christopher.p.mcgurn@gmail.com>
2020-06-29 17:40:37 +0100

    Created object array with placeholder data

--- js/script.js
+++ js/script.js
@@ -3,14 +3,51 @@ Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
 ******************************************/
 
-// For assistance: 
-  // Check the "Project Resources" section of the project instructions
-  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
-
 /*** 
  * `quotes` array 
+ * 
+ * quote: The quote
+ * source: The person or character who said it
+ * citation: A reference to the source of the quote, like the book, movie or song where the quote originates
+ * year: A string or number representing the year the quote originated.
 ***/
-
+const quotes = [
+  {
+    quote: 'Random Quote #1',
+    source: 'Dude A.',
+    citation: 'A Book, Pg. 103',
+    date: 1991,
+    tags: ['motivation', 'life', 'personal development']
+  },
+  {
+    quote: 'Random Quote #2',
+    source: 'Dude B.',
+    citation: 'Unknown',
+    date: 1992,
+    tags: ['music', 'arts', 'inspiration']
+  },
+  {
+    quote: 'Random Quote #3',
+    source: 'Gourley S.',
+    citation: 'HTTP: The definitive Giuide, Pg. 304',
+    date: 1993,
+    tags: ['http', 'internet', 'data transfer']
+  },
+  {
+    quote: 'Random Quote #4',
+    source: 'Ryan D.',
+    citation: 'A Book About Life, Pg. 3',
+    date: 1994,
+    tags: ['life', 'motivation', 'inspiration']
+  },
+  {
+    quote: 'Random Quote #5',
+    source: 'Doolaly O.',
+    citation: 'www.doolaly.com',
+    date: 1995,
+    tags: ['weird', 'til', 'funny']
+  }
+];
 
 
 /***
@@ -30,4 +67,4 @@ project 1 - A Random Quote Generator
  * DO NOT CHANGE THE CODE BELOW!!
 ***/
 
-document.getElementById('load-quote').addEventListener("click", printQuote, false);
+// document.getElementById('load-quote').addEventListener("click", printQuote, false);
